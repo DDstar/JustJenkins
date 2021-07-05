623e99c4b58652c4d76ef49ac305f3f3be5dd617
DDstar <DDstar@jimixiagn.com>
2021-07-05 15:03:56 +0800

    first commit

---
+++ app/src/main/java/com/lifesave/justhelloword/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lifesave.justhelloword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
